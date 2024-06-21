bcb595ad942a2d91d2830826476a72e2cfd0724a
yyf <244190857@qq.com>
2024-06-21 14:19:39 +0800

    typescript

---
+++ index.d.ts
@@ -0,0 +1,48 @@
+// index.d.ts
+declare module 'ckeditor-plugins' {
+    import { Plugin } from '@ckeditor/ckeditor5-core';
+
+    export const Alignment: Plugin;
+    export const Autoformat: Plugin;
+    export const Bold: Plugin;
+    export const Italic: Plugin;
+    export const Strikethrough: Plugin;
+    export const Subscript: Plugin;
+    export const Superscript: Plugin;
+    export const Underline: Plugin;
+    export const BlockQuote: Plugin;
+    export const CodeBlock: Plugin;
+    export const Essentials: Plugin;
+    export const FindAndReplace: Plugin;
+    export const Font: Plugin;
+    export const Heading: Plugin;
+    export const HorizontalLine: Plugin;
+    export const HtmlSupport: Plugin;
+    export const Image: Plugin;
+    export const ImageCaption: Plugin;
+    export const ImageStyle: Plugin;
+    export const ImageToolbar: Plugin;
+    export const ImageResize: Plugin;
+    export const Indent: Plugin;
+    export const Link: Plugin;
+    export const List: Plugin;
+    export const ListProperties: Plugin;
+    export const TodoList: Plugin;
+    export const MediaEmbed: Plugin;
+    export const Paragraph: Plugin;
+    export const PasteFromOffice: Plugin;
+    export const RemoveFormat: Plugin;
+    export const ShowBlocks: Plugin;
+    export const SourceEditing: Plugin;
+    export const Style: Plugin;
+    export const Table: Plugin;
+    export const TableToolbar: Plugin;
+    export const TableCaption: Plugin;
+    export const TableCellProperties: Plugin;
+    export const TableColumnResize: Plugin;
+    export const TableProperties: Plugin;
+    export const Typing: Plugin;
+    export const Undo: Plugin;
+    export const Upload: Plugin;
+    export const WordCount: Plugin;
+}
